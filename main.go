37aed19d3229e4d7593daee236b1e4ce5f61e31d
shinnosuke-K <shin.050195@gmail.com>
2019-05-27 14:11:40 +0900

    add: func writeCSV
    スクレイピングした情報をcsvファイルへ加工して保存

--- main.go
+++ main.go
@@ -1,14 +1,74 @@
 package main
 
 import (
+	"encoding/csv"
 	"log"
 	"net/http"
+	"os"
 	"scraping-school/env"
-
-	"fmt"
+	"strings"
 
 	"github.com/PuerkitoBio/goquery"
 )
+
+var schoolName string
+var course string
+
+func createCSVfile() {
+	if _, err := os.Stat(env.CSVFileName); err != nil {
+		if _, err := os.Create(env.CSVFileName); err != nil {
+			log.Fatal(err)
+		}
+	}
+}
+
+func writeCSV(deviValue string, schoolInfo string) {
+	file, err := os.OpenFile("info.csv", os.O_WRONLY|os.O_APPEND, 0600)
+	defer file.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	schoolInfo = strings.ReplaceAll(schoolInfo, "\n", "")
+	schoolInfo = strings.ReplaceAll(schoolInfo, " ", "")
+
+	for schoolInfo != "" {
+		for _, value := range schoolInfo {
+			if schoolChar := string([]rune{value}); schoolChar == "（" {
+				schoolInfo = strings.Replace(schoolInfo, schoolName, "", 1)
+
+				for _, value := range schoolInfo {
+					if courseChar := string([]rune{value}); courseChar == "）" && strings.Contains(course, "/") {
+						course += courseChar
+						schoolInfo = strings.Replace(schoolInfo, course, "", 1)
+
+						info := []string{
+							deviValue,
+							schoolName,
+							course,
+						}
+
+						writer := csv.NewWriter(file)
+						writer.Write(info)
+
+						writer.Flush()
+
+						schoolName = ""
+						course = ""
+						break
+
+					} else {
+						course += courseChar
+					}
+				}
+				break
+			} else {
+				schoolName += schoolChar
+			}
+		}
+	}
+
+}
 
 func Scrape(url string) {
 	res, err := http.Get(url)
@@ -28,12 +88,12 @@ func Scrape(url string) {
 
 	doc.Find(env.Selector).Each(func(i int, s *goquery.Selection) {
 		deviValue := s.Find(env.DeviValueSelector).Text()
-		schoolName := s.Find(env.SchoolNameSelector).Text()
-		course := s.Find(env.CourseSelector).Text()
-		fmt.Println(deviValue, schoolName, course)
+		schoolInfo := s.Find("td > ul > li").Text()
+		writeCSV(deviValue, schoolInfo)
 	})
 }
 
 func main() {
+	createCSVfile()
 	Scrape(env.SearchURL)
 }
