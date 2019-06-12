b6feaba5be26f60c83e47e16464067f5fc57bcfc
shinnosuke-K <shin.050195@gmail.com>
2019-06-12 12:23:23 +0900

    move func createCSVfile to csv.go

--- csv.go
+++ csv.go
@@ -1,1 +1,14 @@
 package main
+
+import (
+	"log"
+	"os"
+)
+
+func CreateCSVfile(fileName string) {
+	if _, err := os.Stat(fileName); err != nil {
+		if _, err := os.Create(fileName); err != nil {
+			log.Fatal(err)
+		}
+	}
+}
