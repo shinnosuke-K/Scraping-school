1f2140aa564bc04f068281ecbe7d2f99c38abfec
shinnosuke-K <shin.050195@gmail.com>
2019-06-28 18:51:33 +0900

    change arguments
    引数にファイル名を追加

--- scrape/scrape-name-url_test.go
+++ scrape/scrape-name-url_test.go
@@ -10,16 +10,17 @@ func Test_scrapeUrl(t *testing.T) {
 	tests := []struct {
 		name       string
 		schoolName string
+		fileName   string
 		want       string
 	}{
 		// TODO: Add test cases.
-		{"存在する高校名", env.HighSchoolName[0], env.HighSchoolURL[0]},
-		{"海外の高校名", env.HighSchoolName[1], ""},
-		{"存在しない高校名", env.HighSchoolName[2], ""},
+		{"存在する高校名", env.HighSchoolName[0], env.FileName[0], env.HighSchoolURL[0]},
+		{"存在する高校名", env.HighSchoolName[1], env.FileName[1], env.HighSchoolURL[1]},
+		{"同じ名前の高校名", env.HighSchoolName[2], env.FileName[2], env.HighSchoolURL[2]},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := ScrapeUrl(tt.schoolName); got != tt.want {
+			if got := ScrapeUrl(tt.schoolName, tt.fileName); got != tt.want {
 				t.Errorf("scrapeForUrl() = %v, want %v", got, tt.want)
 			}
 		})
